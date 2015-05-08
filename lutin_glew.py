7bc198cb29770c49693777a8b4d57a747093c455
Edouard DUPIN <yui.heero@gmail.com>
2015-05-08 22:44:16 +0200

    [BUILD] update new lutin 0.5.3

--- lutin_glew.py
+++ lutin_glew.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-import lutinModule as module
-import lutinTools as tools
+import lutin.module as module
+import lutin.tools as tools
 
 def get_desc():
 	return "Glew generic glew interface (for windows only)"
@@ -13,14 +13,14 @@ def create(target):
 		
 		myModule.add_export_path(tools.get_current_path(__file__) + "/glew/include/")
 		if target.config["bus-size"] == "32":
-			myModule.add_export_flag_LD([
+			myModule.add_export_flag('link', [
 				tools.get_current_path(__file__) + "/glew/lib/Release/Win32/glew32s.lib"
 				])
 		else:
-			myModule.add_export_flag_LD([
+			myModule.add_export_flag('link', [
 				tools.get_current_path(__file__) + "/glew/lib/Release/x64/glew32s.lib",
 				])
-		myModule.add_export_flag_LD([
+		myModule.add_export_flag('link', [
 			"-lopengl32",
 			"-lgdi32",
 			"-static-libgcc",
