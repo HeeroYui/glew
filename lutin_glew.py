4cd1339927f0c566dda5627155e3734fcd303f29
Edouard DUPIN <yui.heero@gmail.com>
2015-09-24 21:44:04 +0200

    [DEV] update next lutin version

--- lutin_glew.py
+++ lutin_glew.py
@@ -9,25 +9,25 @@ def get_desc():
 def create(target):
 	if target.name=="Windows":
 		#http://glew.sourceforge.net/index.html
-		myModule = module.Module(__file__, 'glew', 'PREBUILD')
+		my_module = module.Module(__file__, 'glew', 'PREBUILD')
 		
-		myModule.add_export_path(tools.get_current_path(__file__) + "/glew/include/")
+		my_module.add_export_path(tools.get_current_path(__file__) + "/glew/include/")
 		if target.config["bus-size"] == "32":
-			myModule.add_export_flag('link', [
+			my_module.add_export_flag('link', [
 				tools.get_current_path(__file__) + "/glew/lib/Release/Win32/glew32s.lib"
 				])
 		else:
-			myModule.add_export_flag('link', [
+			my_module.add_export_flag('link', [
 				tools.get_current_path(__file__) + "/glew/lib/Release/x64/glew32s.lib",
 				])
-		myModule.add_export_flag('link', [
+		my_module.add_export_flag('link', [
 			"-lopengl32",
 			"-lgdi32",
 			"-static-libgcc",
 			"-static-libstdc++"])
 		
 		# add the currrent module at the 
-		return myModule
+		return my_module
 	else:
 		return None
 
