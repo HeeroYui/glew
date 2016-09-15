af13ed0fa2af463de288c40cd0eb136aa9398900
Edouard DUPIN <yui.heero@gmail.com>
2016-09-16 00:13:12 +0200

    [DEV] update v2.0.0

--- lutin_glew.py
+++ lutin_glew.py
@@ -5,7 +5,7 @@ import os
 
 
 def get_type():
-	return "LIBRARY"
+	return "LIBRARY_STATIC"
 
 def get_desc():
 	return "Glew generic glew interface (for windows only)"
@@ -18,7 +18,7 @@ def get_maintainer():
 	        "Marcelo E. Magallon <mmagallo@debian org>"]
 
 def get_version():
-	return [1,13,0]
+	return [2,0,0]
 
 def create(target, module_name):
 	if "Windows" in target.get_type():
@@ -28,7 +28,8 @@ def create(target, module_name):
 		my_module.add_header_file([
 		    'glew/include/GL/glew.h',
 		    'glew/include/GL/glxew.h',
-		    'glew/include/GL/wglew.h'
+		    'glew/include/GL/wglew.h',
+		    'glew/include/GL/eglew.h'
 		    ],
 		    destination_path="GL")
 		my_module.add_src_file([
@@ -37,12 +38,32 @@ def create(target, module_name):
 		    'glew/src/visualinfo.c'
 		    ])
 		"""
+		my_module.add_src_file([
+		    'glew/auto/src/glewinfo_gl.c',
+		    'glew/auto/src/glew_init_tail.c',
+		    'glew/auto/src/glew_head.c',
+		    'glew/auto/src/glew_init_egl.c',
+		    'glew/auto/src/glew_init_gl.c',
+		    'glew/auto/src/glew_init_glx.c',
+		    'glew/auto/src/glew_str_glx.c',
+		    'glew/auto/src/glewinfo_wgl.c',
+		    'glew/auto/src/glewinfo_tail.c',
+		    'glew/auto/src/glew_str_wgl.c',
+		    'glew/auto/src/glew_str_egl.c',
+		    'glew/auto/src/glew_str_tail.c',
+		    'glew/auto/src/glewinfo_head.c',
+		    'glew/auto/src/glew_str_head.c',
+		    'glew/auto/src/glew_init_wgl.c',
+		    'glew/auto/src/glewinfo_egl.c',
+		    'glew/auto/src/glewinfo_glx.c',
+		    ])
+		"""
 		my_module.add_flag('link-lib', [
 		    "opengl32",
 		    "gdi32"
 		    ],
 		    export=True)
-		"""
+		my_module.add_path("glew/auto/src/")
 		return my_module
 	else:
 		return None
